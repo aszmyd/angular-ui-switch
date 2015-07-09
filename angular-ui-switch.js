ec561c30c5913868b723ea4158a568e4a89e89e0
aszmyd <adam.szmyd@polcode.net>
2015-07-09 11:15:38 +0200

    Refactored plugin a little bit, to follow angular styleguide and prepare code for minification and mangle

--- angular-ui-switch.js
+++ angular-ui-switch.js
@@ -1,72 +1,92 @@
-angular.module('uiSwitch', [])
+(function() {
 
-.directive('switch', function($compile, $parse){
-    return {
-          require: 'ngModel'
-        , restrict: 'AE'
-        , replace: true
-        , transclude: true
-        , scope: {
-            isDisabled: '@',
-            ngModel: '=',
-            ngChange: '@'
-        }
-        , controller: function($scope){
-            $scope.updateSwitch = function(element){
-                //set style
-                $scope.setElementStyle(element);
-            };
-            
-            $scope.setElementStyle = function(element){
-                if($scope.ngModel){
-                    element.addClass('checked');
-                }else{
-                    element.removeClass('checked');
-                }
-            };
-        }
-        , template: function(element, attrs) {
+    angular.module('uiSwitch', [])
+        .directive('switch', switchDirective);
+
+    function switchDirective() {
+
+        return {
+            require: 'ngModel',
+             restrict: 'AE',
+             replace: true,
+             transclude: true,
+             scope: {
+                isDisabled: '@',
+                ngModel: '=',
+                ngChange: '@'
+            },
+            controller: controller,
+            template: template,
+            link: link
+        };
+
+        function template (element, attrs) {
+
             var html = '';
-                html += '<span';
-                html +=   ' class="switch' + (attrs.class ? ' ' + attrs.class : '') + '"';
-                html +=   '>';
-                html +=   '<small></small>';
-                html +=   '<input type="checkbox"';
-                html +=     attrs.id ? ' id="' + attrs.id + '"' : '';
-                html +=     attrs.name ? ' name="' + attrs.name + '"' : '';
-                html +=     attrs.ngModel ? ' ng-model="' + attrs.ngModel + '"' : '';
-                html +=     ' style="display:none" />';
-                html +=     '<span class="switch-text">'; //adding new container for switch text
-                html +=     attrs.on ? '<span class="on">'+attrs.on+'</span>' : ''; //switch text on value set by user in directive html markup
-                html +=     attrs.off ? '<span class="off">'+attrs.off + '</span>' : ' ';  //switch text off value set by user in directive html markup
-                html += '</span>';
-                return html;
+            html += '<span';
+            html += ' class="switch' + (attrs.class ? ' ' + attrs.class : '') + '"';
+            html += '>';
+            html += '<small></small>';
+            html += '<input type="checkbox"';
+            html += attrs.id ? ' id="' + attrs.id + '"' : '';
+            html += attrs.name ? ' name="' + attrs.name + '"' : '';
+            html += attrs.ngModel ? ' ng-model="' + attrs.ngModel + '"' : '';
+            html += ' style="display:none" />';
+            html += '<span class="switch-text">'; //adding new container for switch text
+            html += attrs.on ? '<span class="on">' + attrs.on + '</span>' : ''; //switch text on value set by user in directive html markup
+            html += attrs.off ? '<span class="off">' + attrs.off + '</span>' : ' ';  //switch text off value set by user in directive html markup
+            html += '</span>';
+
+            return html;
+
         }
-        , link: function($scope, element, attrs, controller){
+
+        function link($scope, element, attrs) {
             //apply initial style
             $scope.setElementStyle(element);
             //add click event when not disabled
-            if(!angular.isDefined(attrs.isDisabled) || attrs.isDisabled == 'false'){
-                element.bind('click', function(event){
-                    $scope.$apply(function(){
+            if (!angular.isDefined(attrs.isDisabled) || attrs.isDisabled == 'false') {
+                element.bind('click', function (event) {
+                    $scope.$apply(function () {
                         $scope.ngModel = !$scope.ngModel; //toggle
                     });
-                    if($scope.ngChange){
+                    if ($scope.ngChange) {
                         $scope.$parent[$scope.ngChange]();
                     }
                 });
                 //add watch
                 $scope.$watch(
-                    function(){
+                    function () {
                         return $scope.ngModel;
                     },
-                    function(newValue, oldValue){
-                        if(newValue != oldValue){
+                    function (newValue, oldValue) {
+                        if (newValue != oldValue) {
                             $scope.updateSwitch(element);
                         }
                     }
                 );
             }
         }
+
     }
-});
+
+    controller.$inject = ['$scope'];
+
+    function controller($scope) {
+
+        $scope.updateSwitch = function (element) {
+            //set style
+            $scope.setElementStyle(element);
+        };
+
+        $scope.setElementStyle = function (element) {
+            if ($scope.ngModel) {
+                element.addClass('checked');
+            } else {
+                element.removeClass('checked');
+            }
+        };
+    }
+
+
+})();
